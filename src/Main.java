29664d68599a381c3a19db088535dc9897daa649
GlazHorsez <to5gogol@gmail.com>
2024-05-08 15:05:14 +0300

    filler or creator didnt work properly

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        RandomFiller filler= new RandomFiller();
+        List <Toy> toys= new ArrayList<>();
+        filler.fill(toys);
+        Shop shop= new Shop(toys);
+        System.out.println(shop.toys);
     }
 }
